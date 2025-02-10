fea0d3083a08bc0ebf8f4dae22288433ab3c1e6a
Karthick-Anbu12 <anbukarthick006@gmail.com>
2025-02-11 02:51:16 +0530

    front end

--- src/Componets/Navbar.jsx
+++ src/Componets/Navbar.jsx
@@ -12,11 +12,8 @@ function Navbar() {
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark ">
           <div class="container-fluid d-flex ">
             <Link class="navbar-brand " to='/'>Recipe Book</Link>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarText">
-              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <div class=" navbar-light " id="navbarText">
+              <ul class="navbar-nav me-auto mb-2 mb-lg-0 inline">
                 <li class="nav-item ">
                   <Link class="nav-link active " to='/'>Home</Link>
                 </li>
